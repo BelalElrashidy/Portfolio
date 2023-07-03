15875eba54e253dc90280569843ebb8ecf8fe935
BeboElRashidy <bebo.elrashidy22@gmail.com>
2023-07-03 16:31:00 +0300

    change

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
     entry: "./src/main.ts",
     output: {
-        filename: "bundle.js",
+        filename: "Joke.js",
         path: path.resolve(__dirname, "dist"),
     },
     resolve: {
